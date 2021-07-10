225327b4a6a289522dca32d54f37e256f860cffe
mohitpra <56065728+mohitpra@users.noreply.github.com>
2021-07-10 15:16:18 -0400

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,11 +1,11 @@
-
+import Image from 'next/image'
 import {Circle} from 'better-react-spinkit'
 
 function Loading() {
     return (
         <center style={{display:'grid', height: '100vh', placeItems: 'center'}}>
             <div>
-                <img src="https://image.flaticon.com/icons/png/512/124/124034.png"
+                <Image src="https://image.flaticon.com/icons/png/512/124/124034.png"
                 alt=""
                 style={{marginBottom:10}}
                 height={200} 
